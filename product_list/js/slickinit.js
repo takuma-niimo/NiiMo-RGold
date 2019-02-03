d06c65bed8d80662191fdc759db46a302b4f2cc7
takuma-niimo <t.sato@niimo.jp>
2019-02-03 18:22:53 +0900

    ab test

--- product_list/js/slickinit.js
+++ product_list/js/slickinit.js
@@ -1,47 +1,13 @@
 $(document).ready(function(){
-  $('.slider1').slick({
+  $('.slider').slick({
     autoplay: true,
     swipeToSlide: true,
     lazyLoad: 'ondemand',
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: '12%',
-    arrows: false,
-    asNavFor: '.nav1'
-  });
-
-  $('.nav1').slick({
-    swipeToSlide: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    asNavFor: '.slider1',
     centerMode: true,
-    centerPadding: '20%',
-    infinite: true,
-    focusOnSelect: true
-  });
-
-  $('.slider2').slick({
-    autoplay: true,
-    swipeToSlide: true,
-    lazyLoad: 'ondemand',
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: '12%',
-    arrows: false,
-    asNavFor: '.nav2'
-  });
-
-  $('.nav2').slick({
-    swipeToSlide: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider2',
-    centerMode: true,
-    centerPadding: '20%',
-    infinite: true,
-    focusOnSelect: true
+    // centerPadding: '5px',
+    // arrows: false,
+    arrows: true
   });
 });
